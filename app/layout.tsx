f2cff0a7789e0051d4090ac43b9ef02e78024986
deepakvermasnv <deepakvermasnv@gmail.com>
2026-06-26 11:10:53 +0530

    feat: add Google Tag Manager integration
    Install Google Tag Manager to enable analytics and tracking capabilities.

--- app/layout.tsx
+++ app/layout.tsx
@@ -127,7 +127,30 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 
   return (
     <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`} suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PC5WM6ND');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="font-sans antialiased" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PC5WM6ND"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SidebarProvider>
           <script
             id="ld-json"
